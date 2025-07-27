7836b4a1d6a327a49a9518eecceece469b4282f1
Adi-Codes-well <jhaa27211@gmail.com>
2025-07-27 12:30:40 +0530

    master is commiting something

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -20,6 +20,12 @@ import OrdersReceived from './pages/supplier/OrdersReceived';
 import ProductManageCard from './pages/supplier/ProductManageCard';
 
 import ProtectedRoute from './components/ProtectedRoute'; 
+import MyProducts from './pages/supplier/myProduct';
+import VendorPage from './pages/Vendor/VendorPage';
+import SupplierPage from './pages/supplier/SupplierPage';
+import AboutPage from './pages/AboutPage';
+import TrustEnginePage from './pages/TrustEnginePage';
+import OrderManageCard from './pages/supplier/OrderManageCard';
 
 const App = () => {
   return (
@@ -40,7 +46,6 @@ const App = () => {
 
         {/* Supplier Routes */}
         <Route path="/supplier/dashboard" element={<SupplierDashboard />} />
-        {/* <Route path="/supplier/products" element={<ProductManageCard />} /> */}
          <Route path="/supplier/products" element={<MyProducts />} />
         <Route path="/supplier/add-product" element={<AddProductForm />} />
         <Route path="/supplier/orders-received" element={<OrdersReceived />} />
@@ -51,7 +56,7 @@ const App = () => {
         <Route path="/trust-engine" element={<TrustEnginePage />} />
         <Route path="/order-manage" element={<OrderManageCard />} />
       </Route>
-
+      
       <Route path="*" element={<div><h2>404 Page Not Found</h2></div>} />
     </Routes>
   );
