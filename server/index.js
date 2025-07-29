0f889d1220aa5f588fae77f75940d62a082992c3
Adi-Codes-well <jhaa27211@gmail.com>
2025-07-29 18:51:29 +0530

    server op

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path'); 
 const connectDB = require('./config/db');
-+
+
 const authRoutes = require('./routes/authRoutes');
 const supplierRoutes = require('./routes/supplierRoutes');
 const productRoutes = require('./routes/productRoutes');
