275cd1ed270894cf6b00335ed007f32de688535b
Adi-Codes-well <jhaa27211@gmail.com>
2025-07-31 17:14:55 +0530

    Update ProductDetails.jsx

--- client/src/pages/Vendor/ProductDetails.jsx
+++ client/src/pages/Vendor/ProductDetails.jsx
@@ -417,7 +417,7 @@ return (
                 
                     <ArrowLeft className="w-5 h-5 mr-2" />
                     Back to Products
-                </Link></a>
+                </Link>
             </div>
         </div>
 
